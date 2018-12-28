48a2391fd7d72059b621ad92e29626178a2c4a23
Trygve Lie <post@trygve-lie.com>
2018-12-28 01:13:38 +0100

    Initial implementation

---
+++ example/server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const stream = require('readable-stream');
+const Daemon = require('../');
+
+const dest = new stream.Writable({
+    objectMode: true,
+    write(chunk, encoding, callback) {
+        console.log(chunk);
+        callback();
+    },
+});
+
+const daemon = new Daemon();
+daemon.pipe(dest);
+daemon.listen();
